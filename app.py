9dde841eb4a61ea5b31a39fba19b2ce5a570e12c
raffa <raffajogja2019@gmail.com>
2025-01-01 21:06:09 +0700

    modifikasi algoritma

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from joblib import dump, load
 
 app = Flask(__name__)
 
-model = pickle.load(open('trained_model.pkl', 'rb'))
+model = pickle.load(open('trained_rf.pickle', 'rb'))
 
 @app.route("/")
 def home():
@@ -18,24 +18,10 @@ def klasifikasi():
     else:
         new_data = {
         'anxiety_level': [request.form['anxiety_level']],
-        'self_esteem': [request.form['self_esteem']],
-        'mental_health_history': [request.form['mental_health_history']],
         'depression': [request.form['depression']],
-        'headache': [request.form['headache']],
-        'blood_pressure': [request.form['blood_pressure']],
-        'sleep_quality': [request.form['sleep_quality']],
-        'breathing_problem': [request.form['breathing_problem']],
-        'noise_level': [request.form['noise_level']],
-        'living_conditions': [request.form['living_conditions']],
-        'safety': [request.form['safety']],
-        'basic_needs': [request.form['basic_needs']],
-        'academic_performance': [request.form['academic_performance']],
-        'study_load': [request.form['study_load']],
-        'teacher_student_relationship': [request.form['teacher_student_relationship']],
         'future_career_concerns': [request.form['future_career_concerns']],
-        'social_support': [request.form['social_support']],
-        'peer_pressure': [request.form['peer_pressure']],
-        'extracurricular_activities': [request.form['extracurricular_activities']],
+        'headache': [request.form['headache']],
+        'mental_health_history': [request.form['mental_health_history']],
         'bullying': [request.form['bullying']],
     }
         data_df = pd.DataFrame(new_data)
